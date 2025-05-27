d3548f25a6bcb418a71cade66663056f70fd1fae
Moavia Amir <contactmuawia@gmail.com>
2025-05-27 21:45:24 +0500

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AIPyOOP',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Muawiya Amir',
+    description='An AI-powered OOP learning assistant in Python',
+    keywords='OOP Python AI Education',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
